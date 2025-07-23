24f15ba8e5dc3a4b633dfd339c960171a2bb4b55
10pooja112004 <168292150+10pooja112004@users.noreply.github.com>
2025-07-23 20:43:06 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import './App.css';
 
 function App() {
   return (
-    <>
+    <div classname = "container">
       <Navbar />
       <Hero />
       <About />
@@ -21,7 +21,7 @@ function App() {
       <Achievements />
       <Contact />
       <Footer />
-    </>
+    </div>
   );
 }
 
